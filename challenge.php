c2ae3a8b1f178ec0a5fe7291aecb9daed10c086a
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2019-02-11 12:22:03 +1100

    challenge

---
+++ challenge.php
@@ -0,0 +1,56 @@
+<?php 
+// 1. define a class for a car
+
+class Car {
+
+// 2. Properties: brand, model, year, description, weight_kg
+    var $brand;
+    var $model;
+    var $year;
+    var $description;
+    var $weight_kg;
+    var $lbs = 2.20462;
+
+// 3. Methods: name, weight_lbs, set_weight_lbs
+
+function name() {
+return $this->brand . " " . $this->model . " " . $this->year . " " . $this->description . " " . $this->weight_kg;
+}
+
+function weight_lbs() {
+return $this->weight_kg;
+}
+
+function set_weight_lbs() {
+return $this->weight_kg * $this->lbs . " lbs";
+}
+
+}
+
+$car1 = new Car;
+$car1->brand = "Honda";
+$car1->model = "HRV";
+$car1->year = 2018;
+$car1->description = "Test";
+$car1->weight_kg = 24;
+
+$car2 = new Car;
+$car2->brand = "Ford";
+$car2->model = "Focus";
+$car2->year = 2019;
+$car2->description = "Test test test";
+$car2->weight_kg = 35;
+
+echo "<h1>Car's description </h1>" . $car1->name() . "<br/>";
+echo "<h1>Car's weight in kg </h1>" . $car1->weight_lbs() . "<br/>";
+echo "<h1>Weight in lbs</h1>" . $car1->set_weight_lbs() . "<br/>";
+echo "<hr style='color:red'/>";
+echo "<h1>Car's description </h1>" . $car2->name() . "<br/>";
+echo "<h1>Car's weight in kg </h1>" . $car2->weight_lbs() . "<br/>";
+echo "<h1>Weight in lbs</h1>" . $car2->set_weight_lbs() . "<br/>";
+
+$class_methods = get_class_methods("Car");
+echo "<hr/> Class methods:" . implode(", " ,$class_methods) . "<br/>";
+
+
+?>
--- class_methods.php
+++ class_methods.php
@@ -27,11 +27,23 @@ $student1->first_name = "Anna";
 $student1->last_name = "Cook";
 
 
+
+
 $student2 = new Student;
 $student2->first_name = "John";
 $student2->last_name = "Mertz";
 
 echo $student1->full_name() . "<br/>";
-echo $student2->full_name() . "<br/>";
+echo $student2->full_name();
 
+
+//get class methods
+$class_methods = get_class_methods("Student");
+echo "<b> Class methods:" . implode(", ",$class_methods) . "</b><br/>";
+
+if (method_exists("Student", "say_hello")) {
+    echo "Method say_hello() exists in Student class";
+} else {
+    echo "Does not exists";
+}
 ?>
