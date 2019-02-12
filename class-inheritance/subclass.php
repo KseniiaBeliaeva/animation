0895a82bab4fd4a465b6c8893b204e31a5f2ed2c
Kseniia Beliaeva <ksenia.belyaewa@gmail.com>
2019-02-12 12:09:14 +1100

    extend and override

--- class-inheritance/subclass.php
+++ class-inheritance/subclass.php
@@ -1,5 +1,5 @@
-<?php 
-/*inheritance 
+<?php
+/*inheritance
 when a new class takes on the properties and methods of an existing class
 
 Behaviors of a class are shared with subclasses
@@ -12,28 +12,59 @@ Document (parent - superclass)
 |                       |
 V                       V
 ProductBrochure         WarrantyDocument
-            (subclasses)
-*/
+(subclasses)
+ */
 
 //Define a subclass
 
 // class ParentClass {}
 // class Subclass extends ParentClass {}
 
-class User {
+class User
+{
+    var $is_admin = false;
     var $first_name;
     var $last_name;
     var $username;
 
-    function full_name() {
+    function full_name()
+    {
         return $this->first_name . " " . $this->last_name;
     }
 }
 
+class Customer extends User
+{
+    var $city;
+    var $state;
+    var $country;
 
-class Customer extends User {
+    function location() {
+        return $this->city . ", " . $this->state . ", " . $this->country;
+    }
+}
+
+class AdminUser extends User {
+    var $is_admin = true;
+
+    function full_name()
+{
+    return $this->first_name . " " . $this->last_name . " (Admin)." ;
+}
+}
+
+$g = new AdminUser;
+$g->first_name = "Lolla";
+$g->last_name = "Stanly";
+$g->username = "lolla3472";
+echo $g->full_name();
+if (!$g->is_admin) {
+    echo $g->first_name . "is not an Admin";
+} else {
+    echo $g->first_name . "is an Admin";
 
 }
+
 $u = new User;
 $u->first_name = "Jerry";
 $u->last_name = "Sainfeld";
@@ -43,9 +74,16 @@ $c = new Customer;
 $c->first_name = "George";
 $c->last_name = "Costanza";
 $c->username = "gCostanza";
+$c->city = "New York";
+$c->state = "New York";
+$c->country = "United States";
+
 
 echo $u->full_name() . "<br/>";
 echo $c->full_name() . "<br/>";
+echo $c->location() . "<br/>";
+// echo $u->location() . "<br/>"; // no method error
+
 
 echo get_parent_class($u) . "<br/>";
 echo get_parent_class($c) . "<br/>";
@@ -55,5 +93,3 @@ if (is_subclass_of($c, "User")) {
 }
 $parents = class_parents($c);
 echo implode(", ", $parents);
-
-?>
